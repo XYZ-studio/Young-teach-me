91b4277e91cde7674c8d0196ec9ff980b579c72e
xiaoxigua <aijdfajodwsdf@gmail.com>
2021-08-23 19:12:34 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ],
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.html$/,
+                loader: 'html-loader',
+            }
+        ],
+    },
+    plugins: [ // 設定自動插入 main.js
+        new HtmlWebpackPlugin({
+            inject: true,
+            template: `./src/index.html`,
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+    ],
+    devServer: {
+        // contentBase: './dist',
+        port: 8080,
+        hot: true
+    },
+};
