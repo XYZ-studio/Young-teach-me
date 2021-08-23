6f7925a1eedb4e0e4edf826945f3430cd96ddf91
xiaoxigua <aijdfajodwsdf@gmail.com>
2021-08-23 19:57:02 +0800

    add meta

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
-        publicPath: '/Young-teach-me'
+        publicPath: '/Young-teach-me/'
     },
     module: {
         rules: [
