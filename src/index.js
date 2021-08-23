39b816c04424851f8f197a2cc98f94819e042c4c
xiaoxigua <aijdfajodwsdf@gmail.com>
2021-08-23 22:38:40 +0800

    add ??

--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
 import './index.sass';
 
+let count = 0;
+let young = ['KeyY', 'KeyO', 'KeyU', 'KeyN', 'KeyG'];
 
+document.addEventListener('keydown', (event) => {
+    if (count === 5) {
+        document.getElementById('young').className = 'young-close';
+        count = 0;
+    }
+    if (event.code === young[count]) 
+        count++;
+    
+    if (count === 5) {
+        document.getElementById('young').className = 'young-open';
+    }
+
+});
 
