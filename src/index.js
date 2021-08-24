f0258ed02f2adcd1053639935456335279758267
xiaoxigua <aijdfajodwsdf@gmail.com>
2021-08-24 15:48:10 +0800

    add popyoung

--- src/index.js
+++ src/index.js
@@ -1,19 +1,47 @@
 import './index.sass';
+import './pop.sass';
 
-let count = 0;
+let youngCount = 0;
+let popCount = 0;
 let young = ['KeyY', 'KeyO', 'KeyU', 'KeyN', 'KeyG'];
+let popYoung = ['KeyP', 'KeyO', 'KeyP'];
+let popYoungCount = 0;
+let popYoungSwitch = false;
 
 document.addEventListener('keydown', (event) => {
-    if (count === 5) {
+    if (popCount === 5) {
         document.getElementById('young').className = 'young-close';
-        count = 0;
+        popCount = 0;
     }
-    if (event.code === young[count]) 
-        count++;
+    if (youngCount === 3 && event.cod === 'Escape') {
+        document.getElementById('pop-young').className = 'pop-young-close';
+    }
+    if (event.code === young[youngCount])
+        youngCount++;
+    if (event.code === popYoung[popCount])
+        popCount++;
     
-    if (count === 5) {
+    if (youngCount === 5) {
         document.getElementById('young').className = 'young-open';
     }
-
+    if (popCount === 3) {
+        document.getElementById('pop-young').className = 'pop-young-open';
+    }
 });
 
+document.onmouseup = document.onkeypress = (event) => {
+    if (document.getElementById('pop-young').className === 'pop-young-open' && !popYoungSwitch) {
+        popYoungCount++;
+        document.getElementById('count').textContent = popYoungCount;
+        document.getElementById('young-image').className = 'young-image-open';
+        popYoungSwitch = true;
+    }
+};
+
+document.onmousedown = document.onkeyup = (event) => {
+    if (document.getElementById('pop-young').className === 'pop-young-open' && popYoungSwitch) {
+        document.getElementById('count').textContent = popYoungCount;
+        document.getElementById('young-image').className = 'young-image-close';
+        popYoungSwitch = false;
+    }
+};
